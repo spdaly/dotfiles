8eb579777dc11312aa93a9087b42f7b1560374d5
Steve Daly <spdaly@gmail.com>
2020-12-08 13:50:18 -0500

    Update install.sh
    Added install for Fira Code font.

--- install.sh
+++ install.sh
@@ -17,25 +17,7 @@ sudo apt-get install -y \
   unzip \
   apt-transport-https
   
-# Install Jetbrains Mono font
-wget https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip
-sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
-sudo fc-cache -f -v
-
-# Install & Configure Zsh
-if [ "$INSTALL_ZSH" = "true" ]
-then
-    sudo apt-get install -y \
-    fonts-powerline \
-    zsh
-
-    cp -f ~/dotfiles/.zshrc ~/.zshrc
-    chsh -s /usr/bin/zsh $USERNAME
-    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-    echo "source $PWD/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-fi
+sudo apt install fonts-firacode
 
 # Cleanup
 sudo apt-get autoremove -y
